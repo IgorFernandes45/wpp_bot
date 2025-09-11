cfcd8544f923c095fb241e96f95006c7d616e822
igor <iguinbdg@gmail.com>
2025-09-11 16:20:11 -0300

    protegendo banco

--- meu-crud/database.js
+++ meu-crud/database.js
@@ -4,10 +4,10 @@ import mysql from 'mysql2/promise';
 // CONEXÃO COM O BANCO
 // =====================
 export const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'senac',
-  database: 'bancoamnesia',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   decimalNumbers: true
 });
 
